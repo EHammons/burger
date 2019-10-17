a7bc207cb1bb4d8ff8cfe9f213b80a93fddd370f
Stardust <52759497+EHammons@users.noreply.github.com>
2019-10-17 09:44:14 -0500

    added seeds.sql and set up connection

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (burger_name) VALUES ("The Impossible Burger");
+INSERT INTO burger (burger_name) VALUES ("The Beyond");
+INSERT INTO burger (burger_name) VALUES ("Veggie Burger");
+INSERT INTO burger (burger_name) VALUES ("Chesseburger");
+INSERT INTO burger (burger_name) VALUES ("Bacon Jalapeno Cheeseburger");
+INSERT INTO burger (burger_name) VALUES ("Lamb Burger")
